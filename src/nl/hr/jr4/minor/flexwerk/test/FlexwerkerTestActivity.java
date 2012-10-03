317dbb2f6c61d37d03f504c7ddde3fe05c411024
sanderpott <beedeeboy@gmail.com>
2012-10-03 12:49:13 +0200

    Initial commit
    Initial commit

---
+++ src/nl/hr/jr4/minor/flexwerk/test/FlexwerkerTestActivity.java
@@ -0,0 +1,13 @@
+package nl.hr.jr4.minor.flexwerk.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FlexwerkerTestActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
