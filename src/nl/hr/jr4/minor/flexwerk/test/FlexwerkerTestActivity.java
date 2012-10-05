d00d3044a4b07113087e24c77dcaac8acd09a5be
sanderpott <beedeeboy@gmail.com>
2012-10-05 13:00:19 +0200

    onClickListener is handy!
    onClickListener is handy!

--- src/nl/hr/jr4/minor/flexwerk/test/FlexwerkerTestActivity.java
+++ src/nl/hr/jr4/minor/flexwerk/test/FlexwerkerTestActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class FlexwerkerTestActivity extends Activity implements OnClickListener {
 	
@@ -12,6 +13,12 @@ public class FlexwerkerTestActivity extends Activity implements OnClickListener 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button btn1 = (Button) findViewById(R.id.btn_test1);
+        btn1.setOnClickListener(this);
+        Button btn2 = (Button) findViewById(R.id.btn_test2);
+        btn2.setOnClickListener(this);
+        
     }
 
 	public void onClick(View v) {
