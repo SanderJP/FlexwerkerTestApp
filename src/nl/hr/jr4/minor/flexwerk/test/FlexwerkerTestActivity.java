a746af042c96ec65a5a52c7a933018d9bd757d02
sanderpott <beedeeboy@gmail.com>
2012-10-05 12:51:27 +0200

    Added main buttons
    Main buttons for 2 tests

--- src/nl/hr/jr4/minor/flexwerk/test/FlexwerkerTestActivity.java
+++ src/nl/hr/jr4/minor/flexwerk/test/FlexwerkerTestActivity.java
@@ -1,9 +1,12 @@
 package nl.hr.jr4.minor.flexwerk.test;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class FlexwerkerTestActivity extends Activity {
+public class FlexwerkerTestActivity extends Activity implements OnClickListener {
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -11,5 +14,18 @@ public class FlexwerkerTestActivity extends Activity {
         setContentView(R.layout.main);
     }
 
-   
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Intent i;
+		switch (v.getId()) {
+			case R.id.btn_test1 :
+				i = new Intent(this, Test1.class);
+				startActivity(i);
+			break;
+			case R.id.btn_test2 :
+				i = new Intent(this, Test2.class);
+				startActivity(i);
+			break;
+		}
+	}
 }
---
+++ src/nl/hr/jr4/minor/flexwerk/test/Test1.java
@@ -0,0 +1,7 @@
+package nl.hr.jr4.minor.flexwerk.test;
+
+import android.app.Activity;
+
+public class Test1 extends Activity {
+
+}
